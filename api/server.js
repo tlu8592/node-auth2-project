0d8096951d9dd5f1f120c1deda9c01a5530b7352
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-04-19 18:56:56 -0400

    inserted get endpoint for /api and a catch all middleware

--- api/server.js
+++ api/server.js
@@ -14,6 +14,16 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 
+server.get("/api", (req, res) => {
+  res.json({ api: "up" });
+});
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'not found'
+  })
+})
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
